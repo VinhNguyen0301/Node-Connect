79c8098c9d71b4f8d0c8c16326d8bcba2ed1e1ec
VinhNguyen0301 <nguyenvinh.fit@gmail.com>
2025-03-26 11:44:50 +0700

    Fix api key

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,7 +3,7 @@ import "@nomicfoundation/hardhat-toolbox";
 import "@nomicfoundation/hardhat-ethers";
 
 // Import secrets
-const { ALCHEMY_API_KEY, PRIVATE_KEY } = require("./secrets.json");
+const { ALCHEMY_API_KEY, PRIVATE_KEY, ETH_API_KEY } = require("./secrets.json");
 
 const config: HardhatUserConfig = {
   solidity: "0.8.20",  // Using your specified version
@@ -15,7 +15,7 @@ const config: HardhatUserConfig = {
     }
   },
   etherscan: {
-    apiKey: "767D5YCG1Y2YHT6DP9G4RM8N3FIMSF2PAI",
+    apiKey: ETH_API_KEY,
   },
 };
 
